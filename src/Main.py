949ad076a066ffb145871baaab17351ecb17c8fc
Fernando Bombardelli <bombardelli.f@gmail.com>
2014-06-22 12:17:58 -0300

    some adjusts

--- src/Main.py
+++ src/Main.py
@@ -38,7 +38,7 @@ def removePartOfSolution(solution, physicalGraph, virtualEdge, edgeDemand, physi
     );
 
 numberOfCalls = 0;
-CALLS_THREASHOLD = 2000;
+CALLS_THREASHOLD = 10000;
 def mapInitialSolution(virtualEdge, currentSolution, alreadyMappedEdges, gPhysical, gVirtual):
     global numberOfCalls;
     global CALLS_THREASHOLD;
